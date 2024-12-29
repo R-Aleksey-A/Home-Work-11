d409c29536d5e508b084b472858e29cf891b7e84
huawei <romanec.aleksey@gmail.com>
2024-12-29 22:08:48 +0400

    HW11.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Home Work-11");
+    public static void determineYear(int year) {
+        if (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 100 == 0)) && year > 1584) {
 
+            System.out.println("Год является високосным");
+        } else {
+            System.out.println("Год не является високосным");
+        }}
+        public static void main (String[]args){
+            System.out.println("Home Work-11");
+            System.out.println("Задание-1");
+            int year = 2019;
+            {
+                determineYear(year);
+            }
+        }
     }
-}
